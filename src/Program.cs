34f7d13d5915f5d0666bf6db9adaf93ea98d0132
Michael Terry <mterry@sancsoft.com>
2024-08-28 23:00:20 -0400

    new icon and build 4

--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,7 @@ namespace WinSerialPorts
     internal static class Program
     {
         public static string Version = "1.0.0";
-        public static int BuildNumber = 3;
+        public static int BuildNumber = 4;
 
         /// <summary>
         ///  The main entry point for the application.
